114c9c7e8a134632221e4d02ad214e1372009e7f
Anton Murygin <mibus32@gmail.com>
2020-10-29 14:12:09 +0200

    Free pointer

--- lib/tdlib/api.rb
+++ lib/tdlib/api.rb
@@ -18,8 +18,10 @@ module TD::Api
 
   def client_receive(client, timeout)
     update = {}
-    update[:raw] = Dl.td_json_client_receive(client, timeout)
-    return nil if !update[:raw]
+    update[:pointer] = Dl.td_json_client_receive(client, timeout)
+    return nil if update[:pointer].null?
+    update[:raw] = update[:pointer].read_string
+    update[:pointer].free
     update[:extra] = update[:raw].length > 38 ? update[:raw].slice(-38,36) : nil
     # update[:type] = update[:raw].match(/\@type\":\"([a-zA-Z]+)\"/)&.[](1)
     # find type (Without regex! Memory efficient!)
